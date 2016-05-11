bcccf20359405a8c702434988a316b0dc8e2c591
Zed25 <2simonemancini5@gmail.com>
2016-05-11 16:38:44 +0200

    layout

--- app/src/main/java/com/ufos/cyw16/cleanyourworld/MainActivity.java
+++ app/src/main/java/com/ufos/cyw16/cleanyourworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ufos.cyw16.cleanyourworld;
 
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
 
 import com.ufos.cyw16.cleanyourworld.fragment.CalendarFragment;
 
@@ -17,8 +20,6 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager); //Assigns the ViewPager to TabLayout
+        createFragment(); //set ViewPager and TabLayout
     }
 
     //Defines the number of tabs by setting appropriate fragment and tab name
@@ -42,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new CalendarFragment(), "Calendar");
         viewPager.setAdapter(adapter);
+    }
+
+    public void createFragment() {
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        //tabLayout.setTabTextColors(Color.parseColor("#e62b1e"), Color.parseColor("#e62b1e"));
+        tabLayout.setupWithViewPager(viewPager);//Assigns the ViewPager to TabLayout
+
+        setupTabIcons(tabLayout);
+
+    }
+
+    private void setupTabIcons(TabLayout tabLayout) {
+
+        ImageView tabAbout = (ImageView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabAbout.setImageResource(R.drawable.ic_calendar_tab_24dp);
+        tabLayout.getTabAt(0).setCustomView(tabAbout);
     }
 
     //Custom adapter class provides fragments required for the view pager
