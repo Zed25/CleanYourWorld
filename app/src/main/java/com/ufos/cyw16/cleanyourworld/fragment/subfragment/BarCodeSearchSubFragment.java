54d37d2dd0fc716fa5f489273d3337b711ec198e
Zed25 <2simonemancini5@gmail.com>
2016-05-19 13:41:40 +0200

    initial code

--- app/src/main/java/com/ufos/cyw16/cleanyourworld/fragment/subfragment/BarCodeSearchSubFragment.java
+++ app/src/main/java/com/ufos/cyw16/cleanyourworld/fragment/subfragment/BarCodeSearchSubFragment.java
@@ -1,7 +1,10 @@
 package com.ufos.cyw16.cleanyourworld.fragment.subfragment;
 
+
+import android.support.v4.app.Fragment;
+
 /**
  * Created by simone_mancini on 19/05/16.
  */
-public class BarCodeSearchSubFragment {
+public class BarCodeSearchSubFragment extends Fragment {
 }
