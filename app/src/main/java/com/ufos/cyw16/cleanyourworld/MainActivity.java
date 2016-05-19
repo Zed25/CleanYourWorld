8732562029ace7f040aeba3632bb0c939bdf7992
fraottaviano <fr.ottaviano@gmail.com>
2016-05-19 12:08:27 +0200

    layout subfragment

--- app/src/main/java/com/ufos/cyw16/cleanyourworld/MainActivity.java
+++ app/src/main/java/com/ufos/cyw16/cleanyourworld/MainActivity.java
@@ -32,9 +32,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
     private String[] mPlanetTitles = {"prova", "prova2", "prova3"};
-
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
 
@@ -42,23 +40,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setNavigationIcon(R.drawable.ic_menu_24dp);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_24dp); // set menu icon
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         //mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
 
-        getSupportActionBar().setTitle(""); //delete app title
+        getSupportActionBar().setTitle(""); // delete app title
 
         createFragment(); //set ViewPager and TabLayout
 
-        //it manages the left navigation bar
+        // it manages the left navigation bar
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
                 toolbar,
                 R.string.app_name, // nav drawer open - description for accessibility
@@ -79,8 +78,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //Defines the number of tabs by setting appropriate fragment and tab name
+    // Defines the number of tabs by setting appropriate fragment and tab name
     private void setupViewPager(ViewPager viewPager) {
+
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new CalendarFragment(), "Calendar");
         adapter.addFragment(new GeolocFragment(), "Geoloc");
@@ -95,28 +95,32 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         //tabLayout.setTabTextColors(Color.parseColor("#e62b1e"), Color.parseColor("#e62b1e"));
-        tabLayout.setupWithViewPager(viewPager);//Assigns the ViewPager to TabLayout
+        tabLayout.setupWithViewPager(viewPager); //Assigns the ViewPager to TabLayout
 
         setupTabIcons(tabLayout);
 
     }
 
+    // set tab icons (calendar, geolocalization and research) to fragments to improve user interface
     private void setupTabIcons(TabLayout tabLayout) {
 
+        // set calendar fragment icon
         ImageView tabCalendar = (ImageView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
         tabCalendar.setImageResource(R.drawable.ic_calendar_tab_24dp);
         tabLayout.getTabAt(0).setCustomView(tabCalendar);
 
+        // set geolocalization fragment icon
         ImageView tabGeoloc = (ImageView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
         tabGeoloc.setImageResource(R.drawable.ic_geoloc_tab_24dp);
         tabLayout.getTabAt(1).setCustomView(tabGeoloc);
 
+        // set research fragment icon
         ImageView tabSearch = (ImageView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
         tabSearch.setImageResource(R.drawable.ic_search_tab_24dp);
         tabLayout.getTabAt(2).setCustomView(tabSearch);
     }
 
-    //Custom adapter class provides fragments required for the view pager
+    // Custom adapter class provides fragments required for the view pager
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
         private final List<String> mFragmentTitleList = new ArrayList<>();
