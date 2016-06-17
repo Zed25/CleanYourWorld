882ca91ddbe6bc3db732ccd72e15f5bf797ade47
OviDanielB <ovi.daniel.b@gmail.com>
2016-06-17 23:33:13 +0200

    Initial config frame

---
+++ app/src/main/java/com/ufos/cyw16/cleanyourworld/config/ConfigAdapter.java
@@ -0,0 +1,68 @@
+/*
+ * Created by Umberto Ferracci from ovidiudanielbarba and published on 6/15/16 12:37 PM
+ * email:   umberto.ferracci@gmail.com
+ * Project: CleanYourWorld
+ * Package: com.ufos.cyw16.cleanyourworld.config.ConfigAdapter
+ * File name: ConfigAdapter.java
+ * Class name: ConfigAdapter
+ * Last modified: 6/15/16 12:37 PM
+ */
+
+package com.ufos.cyw16.cleanyourworld.config;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ufos.cyw16.cleanyourworld.Models.Regione;
+import com.ufos.cyw16.cleanyourworld.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ovidiudanielbarba on 15/06/16.
+ */
+public class ConfigAdapter extends RecyclerView.Adapter<ConfigAdapter.MyViewHolder>{
+
+    private ArrayList<Regione> regioni;
+
+    public ConfigAdapter(ArrayList<Regione> regioni) {
+        this.regioni = regioni;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.config_row,parent,false);
+
+        MyViewHolder viewHolder = new MyViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Regione regione = regioni.get(position);
+
+        holder.nameTv.setText(regione.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return regioni.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView nameTv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            nameTv = (TextView) itemView.findViewById(R.id.name);
+        }
+    }
+
+
+}
