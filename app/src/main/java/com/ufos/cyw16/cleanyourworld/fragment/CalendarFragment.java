cacddd5967cb62db2f52f8bc154dcdd8bc2444d4
Zed25 <2simonemancini5@gmail.com>
2016-06-26 11:14:20 +0200

    CalendarFragment initialized

--- app/src/main/java/com/ufos/cyw16/cleanyourworld/fragment/CalendarFragment.java
+++ app/src/main/java/com/ufos/cyw16/cleanyourworld/fragment/CalendarFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.ufos.cyw16.cleanyourworld.R;
 
@@ -12,6 +14,8 @@ import com.ufos.cyw16.cleanyourworld.R;
  * Created by simone_mancini on 11/05/16.
  */
 public class CalendarFragment extends Fragment{
+
+    CalendarView calendarView;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -26,7 +30,21 @@ public class CalendarFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.calendar_fragment, container, false); //inflate layout
+        View v = inflater.inflate(R.layout.calendar_fragment, container, false); //inflate layout
+
+        initializeCalendar(v);
+        return v;
+    }
+
+    private void initializeCalendar(View v) {
+        calendarView = (CalendarView) v.findViewById(R.id.clndrView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getContext(), "Date: " + dayOfMonth + " / " + month + " / " + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
